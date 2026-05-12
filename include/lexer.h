0a42ab57c32bbada58d1ef7f03b12091d3cb458c
Julio Cesar Carvalho de Paula Souza <julio191096@gmail.com>
2026-05-11 21:12:25 -0300

    feat: created logic for tokenizing the source
    - Created functions for advancing the ctx, peeking at other tokens and
      matching types of Tokens
    - Chnaged Context and token structure to include a token list and a
      token error type, respectivelly

--- include/lexer.h
+++ include/lexer.h
@@ -1,21 +1,30 @@
 #ifndef LEXER_H
 #define LEXER_H
 
+#define INITIAL_TOKEN_COUNT 32
+
 typedef enum {
-    TOKEN_KEYWORD,
-    TOKEN_SYMBOL,
-    TOKEN_IDENTIFIER,
-    TOKEN_LITERAL_INT,
-    TOKEN_LITERAL_STRING,
-    TOKEN_EOF,
-    TOKEN_ERROR
+	TOKEN_KEYWORD,
+	TOKEN_SYMBOL,
+	TOKEN_IDENTIFIER,
+	TOKEN_LITERAL_INT,
+	TOKEN_LITERAL_STRING,
+	TOKEN_EOF,
+	TOKEN_ERROR
 } TokenType;
 
+typedef enum {
+	LEX_ERR_NONE,
+	LEX_ERR_UNRECOGNIZED_CHAR,
+	// TODO: Add more possible Lexer errors
+} LexerErrorCode;
+
 typedef struct {
-    TokenType type;
-    char* value;
-    int line;
-    int column;
+	TokenType type;
+	char* value;
+	int line;
+	int column;
+	LexerErrorCode error_code;
 } Token;
 
 typedef struct{
@@ -23,13 +32,22 @@ typedef struct{
 	int cursor;
 	int line;
 	int column;
+
+	Token** tokens;
+	int token_count;
+	int token_capacity;
+	int current_idx;
 }LexerCtx;
 
-// Function to create a new token (to be implemented in lexer.c)
 Token* create_token(TokenType type, const char* value, int line, int column);
 
 LexerCtx* init_lexer(const char* source);
+Token* advance_token(LexerCtx* ctx);
+Token* peek_token(LexerCtx* ctx, int offset);
+int match_token(LexerCtx* ctx, TokenType type);
+
 // Function to free token and context memory
+
 void free_context(LexerCtx* ctx);
 void free_token(Token* token);
 
--- src/lexer.c
+++ src/lexer.c
@@ -1,16 +1,18 @@
 #include "lexer.h"
+#include "lang_defs.h"
+#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
 
 
 Token* create_token(TokenType type, const char *value, int line, int column){
-	Token* tk = malloc(sizeof(Token));
-	tk->type = type;
-	tk->value = strdup(value);
-	tk->line = line;
-	tk->column = column;
+	Token* token = malloc(sizeof(Token));
+	token->type = type;
+	token->value = strdup(value);
+	token->line = line;
+	token->column = column;
 
-	return tk;
+	return token;
 }
 
 LexerCtx* init_lexer(const char* source){
@@ -22,11 +24,85 @@ LexerCtx* init_lexer(const char* source){
 	return lexer_ctx;
 }
 
-void free_context(LexerCtx* ctx){
-	if (ctx){
-		free(ctx->source);
-		free(ctx);
+Token* advance_token(LexerCtx* ctx){	
+	char* value = NULL;
+	TokenType type = TOKEN_ERROR;
+	const char tmp[2] = {ctx->source[ctx->cursor], '\0'};	
+	switch (ctx->source[ctx->cursor]){
+		case OPEN_PAR:
+		case CLOSE_PAR:
+		case OPEN_BRACE:
+		case CLOSE_BRACE:
+		case SEMICOLON:
+		case ASSIGN:
+			type = TOKEN_SYMBOL;
+			value = strdup(tmp);
+			break;
+		case '<':
+			if (ctx->source[ctx->cursor + 1] == '<'){
+				value = strdup(BITSHIFT_LEFT);
+				type = TOKEN_SYMBOL;
+				break;
+			}
+			value = strdup(tmp);
+			break;
+		case ':':
+			if (ctx->source[ctx->cursor + 1] == ':'){
+				value = strdup(SCOPE_RESOLUTION);
+				type = TOKEN_SYMBOL;
+				break;
+			}
+			value = strdup(tmp);
+			break;
+		default:
+			if (isdigit(ctx->source[ctx->cursor])){
+				int start = ctx->cursor;
+
+				while (isdigit(ctx->source[ctx->cursor])){
+					ctx->cursor++;
+					ctx->column++;
+				}	
+				int length = ctx->cursor - start;
+				value = strndup(&ctx->source[start], length);
+				break;
+			}else if (isalpha(ctx->source[ctx->cursor]) || ctx->source[ctx->cursor] == '_'){
+				int start = ctx->cursor;
+
+				while (isalpha(ctx->source[ctx->cursor]) || ctx->source[ctx->cursor] == '_'){
+					ctx->cursor++;
+					ctx->column++;
+				}	
+				int length = ctx->cursor - start;
+				value = strndup(&ctx->source[start], length);
+				break;
+			}else if (isspace(ctx->source[ctx->cursor])){
+				while (isspace(ctx->source[ctx->cursor])){
+				if (ctx->source[ctx->cursor] == '\n'){
+					ctx->line++;
+					ctx->column = 1;
+				}else{
+				ctx->column++;}
+				ctx->cursor++;
+				}
+				
+				break;
+			}
 	}
+	if (value != NULL){
+		Token* token = create_token(type, value, ctx->line, ctx->column);
+		free(value);
+		return token; 
+	}
+	free(value);
+	return NULL;
+}
+
+Token* peek_token(LexerCtx* ctx, int offset){
+	return ctx->tokens[ctx->current_idx + offset];
+}
+
+int match_token(LexerCtx *ctx, TokenType type){
+	return (ctx->tokens[ctx->current_idx]->type == type);
 }
 
 void free_token(Token *token){
@@ -35,3 +111,16 @@ void free_token(Token *token){
 		free(token);
 	}
 }
+
+void free_context(LexerCtx* ctx){
+	if (ctx){
+		for (int i = 0; i < ctx->token_count; i++){
+			free_token(ctx->tokens[i]);
+		}
+		free(ctx->tokens);
+		free(ctx->source);
+		free(ctx);
+	}
+}
+
+
