23119f234d8622b891524078e65608d931e5b255
GMayer09 <guilhermecd2005@gmail.com>
2026-05-12 19:56:56 -0300

    feat: teste: created test files

---
+++ test/test_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+void multiplicacao(){
+    int numero1 = 5;
+    int numero2 = 4;
+    int resultado  numero1 * numero2;
+    std::cout << << resultado << std::endl;
+}
+
+void main() {
+    multiplicacao();
+}
